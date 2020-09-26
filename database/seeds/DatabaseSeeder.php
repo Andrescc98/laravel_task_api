df9b5cf20ce08a20ee87a7735c9ace922c261c96
Andrescc98 <andresjosecc@gmail.com>
2020-09-26 08:24:32 -0400

    add roles

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Role;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -13,10 +14,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        Role::create([
+            "role"=>"admin",
+            "description"=>"administrador del sistema"
+        ]);
+
+        Role::create([
+            "role"=>"user",
+            "description"=>"administrador del sistema"
+        ]);
+
         User::create([
             "name"=>"andres",
             "email"=>"ac550807@gmail.com",
             "password"=>bcrypt("12345678"),
+            "role_id"=>1
         ]);
 
         factory(User::class, 50)->create();
